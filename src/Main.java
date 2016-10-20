316235741755ab234312ba3db0b8aa364fa2c1dd
Henri Paves <henri.paves@gmail.com>
2016-10-20 19:36:35 +0300

    smith liigub, smith tapab, neo saab smithi lüüa, kuid smith kukub talle endiselt uuesti tagasi tekkides mõnikord pähe

--- src/Main.java
+++ src/Main.java
@@ -12,48 +12,81 @@ public class Main {
 
         int neo = (int) (plats.length / 2);
         int smith = (int) (Math.random() * plats.length);
-        Scanner klaviatuur = new Scanner(System.in);
-        while (true) {
-            plats[neo] = 1;
-            plats[smith] = 2;
-            System.out.println(Arrays.toString(plats));
-            System.out.println();
-            System.out.println("Sisesta käik (a või d):");
-            String sisend = klaviatuur.nextLine();
-            if (sisend.equals("a")){
-                if (liikumisKontroll(neo-1) == true) {
-                    neo--;
-                    plats[neo + 1] = 0;
+        paheKukkumisKontroll(smith, neo);
+            Scanner klaviatuur = new Scanner(System.in);
+            while (true) {
+                plats[neo] = 1;
+                plats[smith] = 2;
+                System.out.println(Arrays.toString(plats));
+                System.out.println();
+                if (neo == smith) {
+                    System.out.println("Mäng läbi!");
+                    break;
                 } else {
-                    System.out.println("Sa ei saa sinna liikuda.");
-                    System.out.println();
-                }
-            } else if (sisend.equals("d")) {
-                if (liikumisKontroll(neo+1) == true) {
-                    neo++;
-                    plats[neo - 1] = 0;
-                } else {
-                    System.out.println("Sa ei saa sinna liikuda.");
-                    System.out.println(
-                    );
-                }
-            } else {
-                System.out.println("Vale klahv. Väljun mängust.");
-                break;
-            }
+                    System.out.println("Sisesta käik (asd või f):");
+                    String sisend = klaviatuur.nextLine();
+                    if (sisend.equals("a")) {
+                        if (liikumisKontroll(neo - 1) == true) {
+                            neo--;
+                            plats[neo + 1] = 0;
+                        } else {
+                            System.out.println("Sa ei saa sinna liikuda.");
+                            System.out.println();
+                        }
+                    } else if (sisend.equals("d")) {
+                        if (liikumisKontroll(neo + 1) == true) {
+                            neo++;
+                            plats[neo - 1] = 0;
+                        } else {
+                            System.out.println("Sa ei saa sinna liikuda.");
+                            System.out.println();
+                        }
+                    } else if (sisend.equals("s")) {
+                        System.out.println("Jäid paigale");
+                    } else if (sisend.equals("f")) {
+                        if (smith == neo - 1 || smith == neo + 1) {
+                            plats[smith] = 0;
+                            smith = (int) (Math.random() * plats.length);
+                        } else {
+                            System.out.println("Sa ei saa nii kaugelt lüüa.");
+                        }
 
+                    } else {
+                        System.out.println("Vale klahv. Väljun mängust.");
+                        break;
+                    }
+                }
+
+                if (neo < smith) {
+                    smith--;
+                    plats[smith + 1] = 0;
+                } else if (neo > smith) {
+                    smith++;
+                    plats[smith - 1] = 0;
+                }
+
+            }
         }
 
+    private static boolean paheKukkumisKontroll(int smith, int neo) {
+        while (true) {
+            if (smith == neo || smith == neo + 1 || smith == neo - 1) {
+                smith = (int) (Math.random() * plats.length);
+            } else {
+                return false;
+            }
+        }
     }
 
+
     private static boolean liikumisKontroll(int neoSamm) {
-        if (neoSamm == -1) {
-            return false;
-        } else if (neoSamm == plats.length) {
-            return false;
-        } else if (plats[neoSamm] == 2) {
-            return false;
-        } else {
+            if (neoSamm == -1) {
+                return false;
+            } else if (neoSamm == plats.length) {
+                return false;
+            } else if (plats[neoSamm] == 2) {
+                return false;
+            } else {
                 return true;
             }
         }
