45d1a9f08ec4289ee31b6c2830e9ff24be510145
Henri Paves <henri.paves@gmail.com>
2016-11-10 19:41:43 +0200

    neo liigub ja lööb, smith platsis aga teda ei saa mõjutada

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,51 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Text text = new Text(100, 100, "TEST");
+    Text text = new Text(100, 100, "");
     int fieldLength = 800;
     int fieldHeight = 300;
-    int characterWidth = 50;
-    int characterHeight = 100;
 
-    int neoPosition = (fieldLength/2-characterWidth/2);
+    int fighterWidth = 50;
+    int fighterHeight = 100;
 
-    boolean right = false;
+    int strikeWidth = 100;
+    int strikeHeight = 100;
+
+    int neoPosition = (fieldLength/2);
+
+/*    boolean right = false;*/
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Rectangle character = new Rectangle(characterWidth, characterHeight);
-        character.setY(fieldHeight-characterHeight);
+        Rectangle neo = new Rectangle(fighterWidth, fighterHeight);
+        neo.setY(fieldHeight-fighterHeight);
 
+        Rectangle strikeZone = new Rectangle(strikeWidth, strikeHeight);
+        strikeZone.setY(fieldHeight-strikeHeight);
+        strikeZone.setFill(Color.RED);
 
-        character.setX(neoPosition);
+        Rectangle smith = new Rectangle(fighterWidth, fighterHeight);
+        smith.setY(fieldHeight-fighterHeight);
+        smith.setFill(Color.DARKGREY);
+
+        neo.setX(neoPosition);
+        strikeZone.setX(neoPosition);
+        smith.setX(200);
 
         Pane window = new Pane();
         Scene mainView = new Scene(window, fieldLength, fieldHeight);
 
-        window.getChildren().add(character);
         window.getChildren().add(text);
+        window.getChildren().add(neo);
+        window.getChildren().add(smith);
 
         primaryStage.setScene(mainView);
         primaryStage.show();
@@ -40,21 +54,37 @@ public class Main extends Application {
             switch (event1.getCode())           // kontrolli programmi toimimist kasvõi iga kolme rea tagant
             {
                 case A:
-                    neoPosition = neoPosition - 10;
+                    text.setText("A");
+                    neoPosition = neoPosition - 5;
+                    neo.setX(neoPosition);
+                    strikeZone.setX(neoPosition-fighterWidth/2);
                     break;
                 case D:
-                    text.setText("D klahvi vajutati");
+                    text.setText("D");
+                    neoPosition = neoPosition + 5;
+                    neo.setX(neoPosition);
+                    strikeZone.setX(neoPosition-fighterWidth/2);
                     break;
                 case ENTER:
-                    text.setText("Enterit vajutati");
+                    text.setText("Enter");
+                    window.getChildren().add(strikeZone);
+                    mainView.setOnKeyReleased(event -> window.getChildren().remove(strikeZone));
                     break;
             }
         });
 
-        while (neoPosition < fieldLength) {
+        mainView.setOnKeyReleased(event -> text.setText(""));
+
+/*        while (neoPosition < fieldLength) {
             neoPosition++;
-            System.out.println(neoPosition);
-        }
+            neo.setX(neoPosition);
+        }*/
+
+/*        neo.setOnMouseClicked(event -> { // event on sisendparameeter
+                System.out.println("KLIKK"); // kontrolli programmi toimimist kasvõi iga kolme rea tagant
+                neoPosition++;
+            neo.setX(neoPosition);
+        });*/
 
     }
 
