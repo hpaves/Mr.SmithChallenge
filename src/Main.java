71656cf279df8ca6504f9361e60f68e1972c6d8b
Henri Paves <henri.paves@gmail.com>
2016-11-10 16:33:02 +0200

    neo on platsil aga ei liigu

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,75 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Text text = new Text(100, 100, "TEST");
+    int fieldLength = 800;
+    int fieldHeight = 300;
+    int characterWidth = 50;
+    int characterHeight = 100;
+
+    int neoPosition = (fieldLength/2-characterWidth/2);
+
+    boolean right = false;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        VBox esimeseAknaStruktuur = new VBox();
-        Scene sisseLogimisAken = new Scene(esimeseAknaStruktuur, 300, 300);
-        primaryStage.setScene(sisseLogimisAken);
+        Rectangle character = new Rectangle(characterWidth, characterHeight);
+        character.setY(fieldHeight-characterHeight);
+
+
+        character.setX(neoPosition);
+
+        Pane window = new Pane();
+        Scene mainView = new Scene(window, fieldLength, fieldHeight);
+
+        window.getChildren().add(character);
+        window.getChildren().add(text);
+
+        primaryStage.setScene(mainView);
         primaryStage.show();
 
-        Label pealkiri = new Label("Kirjuta parool:");
-        TextField parooliV2li = new TextField();
-        Button sisestusNupp = new Button("Sisene");
-
-        esimeseAknaStruktuur.getChildren().addAll(pealkiri, parooliV2li, sisestusNupp);
-
-        Circle pallike = new Circle(50);
-        Pane sisuPaneel = new Pane();
-        sisuPaneel.getChildren().add(pallike);
-        Scene leheSisu = new Scene(sisuPaneel, 600, 500);
-
-        sisestusNupp.setOnAction(event -> {
-            String parool = parooliV2li.getText();
-            if (parool.equals("salakala")) {
-                System.out.println("Parool on õige");
-                primaryStage.setScene(leheSisu);
-            } else {
-                System.out.println("Parool on vale");
+        mainView.setOnKeyPressed(event1 -> {    // event on sisendparameeter
+            switch (event1.getCode())           // kontrolli programmi toimimist kasvõi iga kolme rea tagant
+            {
+                case A:
+                    neoPosition = neoPosition - 10;
+                    break;
+                case D:
+                    text.setText("D klahvi vajutati");
+                    break;
+                case ENTER:
+                    text.setText("Enterit vajutati");
+                    break;
             }
         });
 
-/*        Pane window = new Pane();
-        Scene view1 = new Scene(window, 400, 300);
+        while (neoPosition < fieldLength) {
+            neoPosition++;
+            System.out.println(neoPosition);
+        }
 
-        primaryStage.setScene(view1);
-        primaryStage.show();
-
-        Circle ring = new Circle(100);
-        ring.setCenterX(100);
-        ring.setCenterY(-50);
-        window.getChildren().add(ring);
-
-        ring.setOnMouseClicked(event -> { // event on sisendparameeter
-            System.out.println("KLIKK"); // kontrolli programmi toimimist kasvõi iga kolme rea tagant
-            ring.setCenterX(200);
-            ring.setCenterY(150);
-        });*/
     }
+
+/*    public void move() {
+        if (neoPosition == fieldLength) {
+            right = true;
+        }
+        if (neoPosition == 0) {
+            right = false;
+        }
+        if (right) {
+            neoPosition++;
+        } else {
+            neoPosition--;
+        }
+
+    }*/
 }
