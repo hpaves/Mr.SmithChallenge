12b0ce09b6566d27f7a06a17b079fbc16a95f42c
Henri Paves <henri.paves@gmail.com>
2016-12-09 21:08:13 +0200

    no red errors, neo invisible

--- src/Fighter.java
+++ src/Fighter.java
@@ -1,14 +1,18 @@
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Fighter {
+public class Fighter extends Rectangle {
 
     public int fighterWidth = 50;
     public int fighterHeight = 100;
     private int position;
 
     public void fighterBlank (int fieldHeight) { 
-        Rectangle fighter = new Rectangle(fighterWidth, fighterHeight);
-        fighter.setY(fieldHeight - fighterHeight);
+        Rectangle fighter = new Rectangle(50, 100);
+//        fighter.setY(fieldHeight - fighterHeight);
+        fighter.setX(300);
+        fighter.setY(100);
+        fighter.setFill(Color.GREEN);
     }
 
     public void setX(int x) {
